a0410cb51816a67c0369961cb4b0ec7d87a2d528
Ian Maia <ian.maia@automattic.com>
2023-07-03 20:35:52 +0200

    Address PR comments to adjust test descriptions and rename plugin field

--- spec/manifest_pr_checker_spec.rb
+++ spec/manifest_pr_checker_spec.rb
@@ -11,41 +11,41 @@ module Danger
     describe 'with Dangerfile' do
       before do
         @dangerfile = testing_dangerfile
-        @my_plugin = @dangerfile.manifest_pr_checker
+        @plugin = @dangerfile.manifest_pr_checker
       end
 
-      it 'returns an error when a PR changed the Gemfile but not Gemfile.lock' do
+      it 'returns an error when a PR changed the Gemfile but not the Gemfile.lock' do
         modified_files = ['Gemfile']
-        allow(@my_plugin.git).to receive(:modified_files).and_return(modified_files)
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
 
-        @my_plugin.check_gemfile_lock_updated
+        @plugin.check_gemfile_lock_updated
 
         expect(@dangerfile.status_report[:errors].count).to eq 1
       end
 
-      it 'returns no errors when both Gemfile and Gemfile.lock were updated' do
+      it 'returns no errors when both the Gemfile and the Gemfile.lock were updated' do
         modified_files = ['Gemfile', 'Gemfile.lock']
-        allow(@my_plugin.git).to receive(:modified_files).and_return(modified_files)
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
 
-        @my_plugin.check_gemfile_lock_updated
+        @plugin.check_gemfile_lock_updated
 
         expect(@dangerfile.status_report[:errors]).to be_empty
       end
 
-      it 'returns an error when a PR changed the Package.swift but not Package.resolved' do
+      it 'returns an error when a PR changed the Package.swift but not the Package.resolved' do
         modified_files = ['Package.swift']
-        allow(@my_plugin.git).to receive(:modified_files).and_return(modified_files)
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
 
-        @my_plugin.check_swift_package_resolved_updated
+        @plugin.check_swift_package_resolved_updated
 
         expect(@dangerfile.status_report[:errors].count).to eq 1
       end
 
-      it 'returns no errors when both Package.swift and Package.resolved were updated' do
+      it 'returns no errors when both the Package.swift and the Package.resolved were updated' do
         modified_files = ['Package.swift', 'Package.resolved']
-        allow(@my_plugin.git).to receive(:modified_files).and_return(modified_files)
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
 
-        @my_plugin.check_swift_package_resolved_updated
+        @plugin.check_swift_package_resolved_updated
 
         expect(@dangerfile.status_report[:errors]).to be_empty
       end
