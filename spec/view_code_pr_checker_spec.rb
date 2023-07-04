d64d0a3aa8546393c4811c59d1cff3078b8ff4f8
Ian Maia <ian.maia@automattic.com>
2023-07-04 19:25:40 +0200

    Reorganise test file Rename `my_plugin` field to just `plugin`; group iOS and Android tests

--- spec/view_code_pr_checker_spec.rb
+++ spec/view_code_pr_checker_spec.rb
@@ -5,99 +5,110 @@ require_relative 'spec_helper'
 module Danger
   describe Danger::ViewCodePRChecker do
     it 'should be a plugin' do
-      expect(Danger::ViewCodePRChecker.new(nil)).to be_a Danger::Plugin
+      expect(described_class.new(nil)).to be_a Danger::Plugin
     end
 
     describe 'with Dangerfile' do
       before do
         @dangerfile = testing_dangerfile
-        @my_plugin = @dangerfile.view_code_pr_checker
+        @plugin = @dangerfile.view_code_pr_checker
       end
 
-      it 'warns when a PR with views do not have screenshots' do
+      it 'warns when an iOS PR with views does not have screenshots' do
         modified_files = ['TestView.swift', 'SimpleViewHelper.m']
-        allow(@my_plugin.git).to receive(:modified_files).and_return(modified_files)
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
 
-        allow(@my_plugin.github).to receive(:pr_body).and_return('PR Body')
+        allow(@plugin.github).to receive(:pr_body).and_return('PR Body')
 
-        @my_plugin.view_changes_need_screenshots
-
-        expect(@dangerfile.status_report[:warnings].count).to eq 1
-      end
-
-      it 'warns when an Android PR with views do not have screenshots' do
-        modified_files = ['test_view.xml', 'SimpleViewHelper.kt']
-        allow(@my_plugin.git).to receive(:modified_files).and_return(modified_files)
-
-        allow(@my_plugin.github).to receive(:pr_body).and_return('PR Body')
-
-        @my_plugin.view_changes_need_screenshots
+        @plugin.view_changes_need_screenshots
 
         expect(@dangerfile.status_report[:warnings].count).to eq 1
       end
 
-      it 'warns when a PR with buttons do not have screenshots' do
+      it 'warns when an iOS PR with buttons does not have screenshots' do
         modified_files = ['SimpleViewHelper.m', 'MyAwesomeButton.swift']
-        allow(@my_plugin.git).to receive(:modified_files).and_return(modified_files)
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
 
-        allow(@my_plugin.github).to receive(:pr_body).and_return('PR Body')
+        allow(@plugin.github).to receive(:pr_body).and_return('PR Body')
 
-        @my_plugin.view_changes_need_screenshots
-
-        expect(@dangerfile.status_report[:warnings].count).to eq 1
-      end
-
-      it 'warns when an Android PR with buttons do not have screenshots' do
-        modified_files = ['SimpleViewHelper.kt', 'MyAwesomeButton.java']
-        allow(@my_plugin.git).to receive(:modified_files).and_return(modified_files)
-
-        allow(@my_plugin.github).to receive(:pr_body).and_return('PR Body')
-
-        @my_plugin.view_changes_need_screenshots
+        @plugin.view_changes_need_screenshots
 
         expect(@dangerfile.status_report[:warnings].count).to eq 1
       end
 
-      it 'does nothing when a PR has views but has screenshots' do
+      it 'does nothing when an iOS PR has views but has screenshots' do
         modified_files = ['TestView.swift', 'SimpleViewHelper.m']
-        allow(@my_plugin.git).to receive(:modified_files).and_return(modified_files)
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
 
-        allow(@my_plugin.github).to receive(:pr_body).and_return('PR [![Alt text](https://myimages.com/boo)](https://digitalocean.com) Body')
+        allow(@plugin.github).to receive(:pr_body).and_return('PR [![Alt text](https://myimages.com/boo)](https://digitalocean.com) Body')
 
-        @my_plugin.view_changes_need_screenshots
+        @plugin.view_changes_need_screenshots
 
         expect(@dangerfile.status_report[:warnings]).to be_empty
+      end
+
+      it 'does nothing when an iOS PR has buttons but has screenshots' do
+        modified_files = ['SimpleViewHelper.m', 'MyButton.swift']
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
+
+        allow(@plugin.github).to receive(:pr_body).and_return('PR <img src=\'https://myimages.com/boo.png\' /> Body')
+
+        @plugin.view_changes_need_screenshots
+
+        expect(@dangerfile.status_report[:warnings]).to be_empty
+      end
+
+      it 'does nothing when an iOS PR does not have view code' do
+        modified_files = ['SimpleViewHelper.m', 'MyButtonTester.swift']
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
+
+        allow(@plugin.github).to receive(:pr_body).and_return('Body')
+
+        @plugin.view_changes_need_screenshots
+
+        expect(@dangerfile.status_report[:warnings]).to be_empty
+      end
+
+      it 'warns when an Android PR with views does not have screenshots' do
+        modified_files = ['test_view.xml', 'SimpleViewHelper.kt']
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
+
+        allow(@plugin.github).to receive(:pr_body).and_return('PR Body')
+
+        @plugin.view_changes_need_screenshots
+
+        expect(@dangerfile.status_report[:warnings].count).to eq 1
+      end
+
+      it 'warns when an Android PR with buttons does not have screenshots' do
+        modified_files = ['SimpleViewHelper.kt', 'MyAwesomeButton.java']
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
+
+        allow(@plugin.github).to receive(:pr_body).and_return('PR Body')
+
+        @plugin.view_changes_need_screenshots
+
+        expect(@dangerfile.status_report[:warnings].count).to eq 1
       end
 
       it 'does nothing when an Android PR has views but has screenshots' do
         modified_files = ['TestView.kt', 'SimpleViewHelper.java']
-        allow(@my_plugin.git).to receive(:modified_files).and_return(modified_files)
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
 
-        allow(@my_plugin.github).to receive(:pr_body).and_return('PR [![Alt text](https://myimages.com/boo)](https://digitalocean.com) Body')
+        allow(@plugin.github).to receive(:pr_body).and_return('PR [![Alt text](https://myimages.com/boo)](https://digitalocean.com) Body')
 
-        @my_plugin.view_changes_need_screenshots
-
-        expect(@dangerfile.status_report[:warnings]).to be_empty
-      end
-
-      it 'does nothing when a PR has buttons but has screenshots' do
-        modified_files = ['SimpleViewHelper.m', 'MyButton.swift']
-        allow(@my_plugin.git).to receive(:modified_files).and_return(modified_files)
-
-        allow(@my_plugin.github).to receive(:pr_body).and_return('PR <img src=\'https://myimages.com/boo.png\' /> Body')
-
-        @my_plugin.view_changes_need_screenshots
+        @plugin.view_changes_need_screenshots
 
         expect(@dangerfile.status_report[:warnings]).to be_empty
       end
 
-      it 'does nothing when a PR doesn not have view code' do
-        modified_files = ['SimpleViewHelper.m', 'MyButtonTester.swift']
-        allow(@my_plugin.git).to receive(:modified_files).and_return(modified_files)
+      it 'does nothing when an Android PR does not have view code' do
+        modified_files = ['SimpleViewHelper.java', 'MyButtonTester.kt']
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
 
-        allow(@my_plugin.github).to receive(:pr_body).and_return('Body')
+        allow(@plugin.github).to receive(:pr_body).and_return('Body')
 
-        @my_plugin.view_changes_need_screenshots
+        @plugin.view_changes_need_screenshots
 
         expect(@dangerfile.status_report[:warnings]).to be_empty
       end
