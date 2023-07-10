d2597eedfcf3883b4d2c29a63dcdaf6a8fa76410
Ian Maia <ian.maia@automattic.com>
2023-07-10 18:59:21 +0200

    Fix Rubocop offenses

--- lib/dangermattic/plugins/manifest_pr_checker.rb
+++ lib/dangermattic/plugins/manifest_pr_checker.rb
@@ -3,8 +3,7 @@
 module Danger
   # Plugin to check if the Gemfile.lock was updated when changing the Gemfile in a PR.
   class ManifestPRChecker < Plugin
-
-    # Check if the `Gemfile` file was modified without a corresponding `Gemfile.lock` update 
+    # Check if the `Gemfile` file was modified without a corresponding `Gemfile.lock` update
     def check_gemfile_lock_updated
       check_manifest_lock_updated(
         file_name: 'Gemfile',
@@ -13,16 +12,16 @@ module Danger
       )
     end
 
-      # Check if the `Podfile` file was modified without a corresponding `Podfile.lock` update 
-      def check_podfile_lock_updated
-        check_manifest_lock_updated(
-          file_name: 'Podfile',
-          lock_file_name: 'Podfile.lock',
-          instruction: 'Please run `bundle exec pod install`'
-        )
-      end
-    
-    # Check if the `Package.swift` file was modified without a corresponding `Package.resolved` update 
+    # Check if the `Podfile` file was modified without a corresponding `Podfile.lock` update
+    def check_podfile_lock_updated
+      check_manifest_lock_updated(
+        file_name: 'Podfile',
+        lock_file_name: 'Podfile.lock',
+        instruction: 'Please run `bundle exec pod install`'
+      )
+    end
+
+    # Check if the `Package.swift` file was modified without a corresponding `Package.resolved` update
     def check_swift_package_resolved_updated
       check_manifest_lock_updated(
         file_name: 'Package.swift',
@@ -36,10 +35,10 @@ module Danger
     def check_manifest_lock_updated(file_name:, lock_file_name:, instruction:)
       manifest_modified = git.modified_files.include?(file_name)
       lock_modified = git.modified_files.include?(lock_file_name)
-      
-      if manifest_modified && !lock_modified
-        warn("#{file_name} was changed without updating #{lock_file_name}. #{instruction}.")
-      end
+
+      return unless manifest_modified && !lock_modified
+
+      warn("#{file_name} was changed without updating #{lock_file_name}. #{instruction}.")
     end
   end
 end
