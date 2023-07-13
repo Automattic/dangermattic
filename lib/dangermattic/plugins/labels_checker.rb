ed4a338cbe8080641b7acd3347c441409d85ac36
Ian Maia <ian.maia@automattic.com>
2023-07-13 19:45:00 +0200

    Update code to allow for a list of 'do not merge' labels and dont use defaults for required labels and require labels warning

--- lib/dangermattic/plugins/labels_checker.rb
+++ lib/dangermattic/plugins/labels_checker.rb
@@ -3,39 +3,47 @@
 module Danger
   # Plugin to check for labels in a PR.
   class LabelsChecker < Plugin
-    DEFAULT_DO_NOT_MERGE_LABEL = 'Do Not Merge'
-    DEFAULT_LABELS_WARNING = 'Please add the required labels for this project.'
-    DEFAULT_REQUIRED_LABELS = [/.*\S.*/].freeze
+    DEFAULT_DO_NOT_MERGE_LABELS = [
+      'Do Not Merge'
+    ]
 
-    # Checks if a PR is missing labels or is marked as 'do not merge'.
+    # Checks if a PR is missing labels or is marked with labels for not merging.
+    # If labels are missing, the plugin will emit a warning. If a label indicating that the PR should not be merged is present,
+    # an error will be emitted, preventing the final PR merge.
     #
-    # @param do_not_merge_label [String] The label indicating that a merge should not be performed.
-    #   Defaults to DEFAULT_DO_NOT_MERGE_LABEL if not provided.
-    # @param required_labels [Array<String>] The list of labels that must be present for a merge operation to proceed.
+    # @param do_not_merge_labels [String] The possible labels indicating that a merge should not be allowed.
+    #   Defaults to DEFAULT_DO_NOT_MERGE_LABELS if not provided.
+    # @param required_labels [Array<String>] The list of RegExes that will prevent the merge if any matching label is present in the PR.
     #   Defaults to an empty array if not provided.
     # @param required_labels_warning [String] The warning message displayed if the required labels are not present.
-    #   Defaults to DEFAULT_LABELS_WARNING if not provided.
+    #   Defaults to showing the provided label regexes.
     #
     # @return [void]
-    def check(do_not_merge_label: DEFAULT_DO_NOT_MERGE_LABEL, required_labels: DEFAULT_REQUIRED_LABELS,
-              required_labels_warning: DEFAULT_LABELS_WARNING)
+    def check(do_not_merge_labels: DEFAULT_DO_NOT_MERGE_LABELS, required_labels: [], required_labels_warning: nil)
       github_labels = danger.github.pr_labels
 
-      pr_has_all_required_labels = github_labels.all? do |pr_label|
-        required_labels.any? do |required_label|
-          pr_label =~ required_label
-        end
+      # warn if a PR matches any of the
+      missing_required_labels = required_labels.reject do |required_label|
+        github_labels.any? { |pr_label| pr_label =~ required_label }
       end
 
-      if github_labels.empty?
-        warn('PR is missing at least one label.')
-      elsif !pr_has_all_required_labels
-        warn(required_labels_warning)
+      unless missing_required_labels.empty?
+        missing_labels_str_list = missing_required_labels.map { |regex| regex.source }
+        warn(required_labels_warning || "PR is missing label(s) matching: #{csv_markdown_list(missing_labels_str_list)}")
       end
 
       # A PR shouldn't be merged with the 'DO NOT MERGE' label.
-      found_label = github_labels.find { |label| do_not_merge_label.casecmp?(label) }
-      failure("This PR is tagged with '#{found_label}' label.") if found_label
+      found_labels = github_labels.select do |github_label|
+        do_not_merge_labels.any? { |label| github_label.casecmp?(label) }
+      end
+
+      failure("This PR is tagged with #{csv_markdown_list(found_labels)} label(s).") unless found_labels.empty?
+    end
+
+    private
+
+    def csv_markdown_list(items)
+      items.map { |item| "`#{item}`" }.join(", ")
     end
   end
 end
--- spec/labels_checker_spec.rb
+++ spec/labels_checker_spec.rb
@@ -14,21 +14,21 @@ module Danger
         @plugin = @dangerfile.labels_checker
       end
 
-      it 'returns a warning when a PR does not have labels' do
-        pr_labels = []
-        allow(@plugin.github).to receive(:pr_labels).and_return(pr_labels)
+      it 'returns a warning when a PR does not have at least one label' do
+        allow(@plugin.github).to receive(:pr_labels).and_return([])
 
-        @plugin.check
+        warning = 'PR is missing at least one label.'
+        @plugin.check(required_labels: [/.*/], required_labels_warning: warning)
 
         expect(@dangerfile.status_report[:errors]).to be_empty
-        expect(@dangerfile.status_report[:warnings]).to eq(['PR is missing at least one label.'])
+        expect(@dangerfile.status_report[:warnings]).to eq([warning])
       end
 
       it 'does nothing when a PR has at least one label' do
         pr_labels = ['my-label']
         allow(@plugin.github).to receive(:pr_labels).and_return(pr_labels)
 
-        @plugin.check
+        @plugin.check(required_labels: [/.*/])
 
         expect(@dangerfile.status_report[:errors]).to be_empty
         expect(@dangerfile.status_report[:warnings]).to be_empty
@@ -41,19 +41,20 @@ module Danger
         @plugin.check
 
         expect(@dangerfile.status_report[:warnings]).to be_empty
-        expect(@dangerfile.status_report[:errors]).to eq(["This PR is tagged with '#{pr_label}' label."])
+        expect(@dangerfile.status_report[:errors]).to eq(["This PR is tagged with `#{pr_label}` label(s)."])
       end
 
       it 'returns an error when a PR has a custom label for not merging' do
         pr_label = 'please dont merge'
         allow(@plugin.github).to receive(:pr_labels).and_return([pr_label])
 
+        allowed_labels = [pr_label, 'blocked']
         @plugin.check(
-          do_not_merge_label: pr_label
+          do_not_merge_labels: allowed_labels
         )
 
         expect(@dangerfile.status_report[:warnings]).to be_empty
-        expect(@dangerfile.status_report[:errors]).to eq(["This PR is tagged with '#{pr_label}' label."])
+        expect(@dangerfile.status_report[:errors]).to eq(["This PR is tagged with `#{pr_label}` label(s)."])
       end
 
       it 'returns a warning when a PR has custom label requirements' do
@@ -65,7 +66,7 @@ module Danger
         )
 
         expect(@dangerfile.status_report[:errors]).to be_empty
-        expect(@dangerfile.status_report[:warnings]).to eq(['Please add the required labels for this project.'])
+        expect(@dangerfile.status_report[:warnings]).to eq(['PR is missing label(s) matching: `feature: .*`'])
       end
 
       it 'returns a custom warning when a PR has custom label requirements' do
