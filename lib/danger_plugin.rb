9ef0412985b4613a94efb2db1ecb6558b1337ffa
Ian Maia <ian.maia@automattic.com>
2023-07-13 12:12:44 +0200

    Update to automatically require all plugins under `dangermattic/plugins`

--- lib/danger_plugin.rb
+++ lib/danger_plugin.rb
@@ -1,8 +1,3 @@
 # frozen_string_literal: true
 
-require 'dangermattic/plugins/android_unit_test_checker'
-require 'dangermattic/plugins/labels_pr_checker'
-require 'dangermattic/plugins/manifest_pr_checker'
-require 'dangermattic/plugins/milestone_checker'
-require 'dangermattic/plugins/pr_size_checker'
-require 'dangermattic/plugins/view_changes_need_screenshots'
+Dir[File.join(__dir__, 'dangermattic/plugins', '*.rb')].each { |file| require file }
