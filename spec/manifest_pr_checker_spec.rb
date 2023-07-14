fff1a0c773d737a22796b0bcf9a99c0198d81061
Ian Maia <ian.maia@automattic.com>
2023-07-14 17:18:52 +0200

    Make the expectations a bit more strict by checking the actual warning content

--- spec/manifest_pr_checker_spec.rb
+++ spec/manifest_pr_checker_spec.rb
@@ -20,11 +20,21 @@ module Danger
 
         @plugin.check_gemfile_lock_updated
 
-        expect(@dangerfile.status_report[:warnings].count).to eq 1
+        expected_warning = ['Gemfile was changed without updating Gemfile.lock. Please run `bundle install` or `bundle update <updated_gem>`.']
+        expect(@dangerfile.status_report[:warnings]).to eq expected_warning
       end
 
       it 'returns no warnings when both the Gemfile and the Gemfile.lock were updated' do
         modified_files = ['Gemfile', 'Gemfile.lock']
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
+
+        @plugin.check_gemfile_lock_updated
+
+        expect(@dangerfile.status_report[:warnings]).to be_empty
+      end
+
+      it 'returns no warnings when only the Gemfile.lock was updated' do
+        modified_files = ['Gemfile.lock']
         allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
 
         @plugin.check_gemfile_lock_updated
@@ -38,11 +48,21 @@ module Danger
 
         @plugin.check_podfile_lock_updated
 
-        expect(@dangerfile.status_report[:warnings].count).to eq 1
+        expected_warning = ['Podfile was changed without updating Podfile.lock. Please run `bundle exec pod install`.']
+        expect(@dangerfile.status_report[:warnings]).to eq expected_warning
       end
 
       it 'returns no warnings when both the Podfile and the Podfile.lock were updated' do
         modified_files = ['Podfile', 'Podfile.lock']
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
+
+        @plugin.check_podfile_lock_updated
+
+        expect(@dangerfile.status_report[:warnings]).to be_empty
+      end
+
+      it 'returns no warnings when only the Podfile.lock was updated' do
+        modified_files = ['Podfile.lock']
         allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
 
         @plugin.check_podfile_lock_updated
@@ -56,11 +76,21 @@ module Danger
 
         @plugin.check_swift_package_resolved_updated
 
-        expect(@dangerfile.status_report[:warnings].count).to eq 1
+        expected_warning = ['Package.swift was changed without updating Package.resolved. Please resolve the Swift packages in Xcode.']
+        expect(@dangerfile.status_report[:warnings]).to eq expected_warning
       end
 
       it 'returns no warnings when both the Package.swift and the Package.resolved were updated' do
         modified_files = ['Package.swift', 'Package.resolved']
+        allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
+
+        @plugin.check_swift_package_resolved_updated
+
+        expect(@dangerfile.status_report[:warnings]).to be_empty
+      end
+
+      it 'returns no warnings when only the Package.resolved was updated' do
+        modified_files = ['Package.resolved']
         allow(@plugin.git).to receive(:modified_files).and_return(modified_files)
 
         @plugin.check_swift_package_resolved_updated
