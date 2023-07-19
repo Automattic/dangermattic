e97e3d9c33cccaca88770c7f693a8278cfca68ea
Ian Maia <ian.maia@automattic.com>
2023-07-19 20:39:13 +0200

    Add initial code for ios/android miscellaneous checks plugin

---
+++ lib/dangermattic/plugins/common/git_utils.rb
@@ -0,0 +1,93 @@
+# frozen_string_literal: true
+
+module Danger
+  # Utility plugin with useful methods related to git
+  class GitUtils < Plugin
+    # Check added lines in a PR for a specific pattern and issue a warning or failure message when found.
+    #
+    # @param file_selector [Proc] A block to select the files in the PR.
+    #   The block should take a file path as input and return true if the file should be checked.
+    #
+    # @param matcher [Proc] A block that will select the diff lines to report.
+    #   The block should take a line of text as input and return true if the line matches the pattern.
+    #
+    # @param message [String] The warning or failure message to display when the pattern is found in a line.
+    #
+    # @param fail_on_error [Boolean] (optional) When set to true, whenever a line matches the criteria, the method will emit an error, when false, emit a warning. Default is false.
+    #
+    # @example Checking for added lines containing 'FIXME' and failing the build:
+    #   check_added_diff_lines(file_selector: ->(path) { File.extname(path) == ('.swift') }, line_matcher: ->(line) { line.include?("FIXME") }, message: "A FIXME was added, failing build.", fail_on_error: true)
+    #
+    def check_added_diff_lines(file_selector:, line_matcher:, message:, fail_on_error: false)
+      modified_files = added_and_modified_files.select(&file_selector)
+
+      modified_files.each do |file|
+        diff = danger.git.diff_for_file(file)
+
+        diff.patch.each_line do |line|
+          next unless change_type(diff_line: line) == :added
+          next unless line_matcher.call(line)
+
+          final_message = <<~MESSAGE
+            #{message}
+            File `#{file}`:
+            ```diff
+            #{line.chomp}
+            ```
+          MESSAGE
+
+          if fail_on_error
+            failure(final_message)
+          else
+            warn(final_message)
+          end
+        end
+      end
+    end
+
+    # Determine the type of change for a given line in a git diff.
+    #
+    # @param diff_line [String] The line from a git diff that needs to be classified.
+    #
+    # @example Determining the change type of a diff line:
+    #   change_type(diff_line: "+ new line added")
+    #   #=> :added
+    #
+    #   change_type(diff_line: "- line removed")
+    #   #=> :removed
+    #
+    #   change_type(diff_line: " context line")
+    #   #=> :other
+    #
+    # @return [Symbol] The type of change for the given diff line. Possible values are:
+    #   - :added for added lines
+    #   - :removed for removed lines
+    #   - :other for any other type of lines
+    #
+    def change_type(diff_line:)
+      if diff_line.start_with?('+') && !diff_line.start_with?('+++ ')
+        :added
+      elsif diff_line.start_with?('-') && !diff_line.start_with?('--- ')
+        :removed
+      else
+        :other
+      end
+    end
+
+    # Get the list of added and modified files in the current Pull Request.
+    #
+    # @return [Array<String>] An array containing the file paths of added and modified files.
+    #
+    def added_and_modified_files
+      danger.git.added_files + danger.git.modified_files
+    end
+
+    # Get the list of all additions, changes and deletions in the current Pull Request.
+    #
+    # @return [Array<String>] An array containing the file paths of all modified files.
+    #
+    def all_modified_files
+      danger.git.added_files + danger.git.modified_files + danger.git.deleted_files
+    end
+  end
+end
---
+++ spec/android_misc_checks_spec.rb
@@ -0,0 +1,191 @@
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+
+module Danger
+  describe Danger::AndroidMiscChecks do
+    it 'is a plugin' do
+      expect(described_class.new(nil)).to be_a Danger::Plugin
+    end
+
+    describe 'with Dangerfile' do
+      before do
+        @dangerfile = testing_dangerfile
+        @plugin = @dangerfile.android_misc_checks
+
+        allow(@plugin.git).to receive(:added_files).and_return([])
+        allow(@plugin.git).to receive(:modified_files).and_return([])
+        allow(@plugin.git).to receive(:deleted_files).and_return([])
+
+        stub_const('GitDiffStruct', Struct.new(:type, :path, :patch))
+      end
+
+      context 'when changing strings.xml files' do
+        it 'returns a warning when a PR adds a string resource reference inside a strings.xml file' do
+          strings_xml_path = './src/main/res/values/strings.xml'
+          allow(@plugin.git).to receive(:modified_files).and_return([strings_xml_path])
+
+          strings_xml_diff = <<~STRINGS
+            diff --git a/src/main/res/values/strings.xml b/src/main/res/values/strings.xml
+            index 5794d472..772e2b99 100644
+            --- a/src/main/res/values/strings.xml
+            +++ b/src/main/res/values/strings.xml
+            @@ -1,3 +1,6 @@
+             <?xml version="1.0" encoding="UTF-8"?>
+             <resources xmlns:tools="http://schemas.android.com/tools">
+            +  <string name="select_categories">Select categories</string>
+            +  <string name="video_quality">Video Quality</string>
+            +  <string name="screen_title">@string/app_name</string>
+             </resources>
+          STRINGS
+
+          diff = GitDiffStruct.new('modified', strings_xml_path, strings_xml_diff)
+
+          allow(@plugin.git).to receive(:diff_for_file).with(strings_xml_path).and_return(diff)
+
+          @plugin.check_strings_do_not_refer_resource
+
+          expected_warning = <<~WARNING
+            This PR adds a translatable entry which references another string resource; this usually causes issues with translations.
+            Please make sure to set the `translatable="false"` attribute:
+            File `#{strings_xml_path}`:
+            ```diff
+            +  <string name="screen_title">@string/app_name</string>
+            ```
+          WARNING
+
+          expect(@dangerfile.status_report[:warnings]).to eq [expected_warning]
+        end
+
+        it 'returns multiple warnings when a PR adds multiple string resource references inside multiple strings.xml files' do
+          main_strings_xml = './src/main/res/values/strings.xml'
+          ptbr_strings_xml = './src/main/res/values-pt-rBR/strings.xml'
+          strings_xml_paths = [main_strings_xml, ptbr_strings_xml]
+          allow(@plugin.git).to receive(:modified_files).and_return(strings_xml_paths)
+
+          main_xml_diff = <<~STRINGS
+            diff --git a/src/main/res/values/strings.xml b/src/main/res/values/strings.xml
+            index 5794d472..772e2b99 100644
+            --- a/src/main/res/values/strings.xml
+            +++ b/src/main/res/values/strings.xml
+            @@ -1,3 +1,6 @@
+             <?xml version="1.0" encoding="UTF-8"?>
+             <resources xmlns:tools="http://schemas.android.com/tools">
+            +  <string name="select_categories">Select categories</string>
+            +  <string name="video_quality">Video Quality</string>
+            +  <string name="screen_title">@string/app_name</string>
+            +  <string name="screen_button">@string/button</string>
+            -  <string name="field_hint">@string/hint</string>
+             </resources>
+          STRINGS
+
+          main_diff = GitDiffStruct.new('modified', main_strings_xml, main_xml_diff)
+
+          allow(@plugin.git).to receive(:diff_for_file).with(main_strings_xml).and_return(main_diff)
+
+          ptbr_xml_diff = <<~STRINGS
+            diff --git a/src/main/res/values-pt-rBR/strings.xml b/src/main/res/values-pt-rBR/strings.xml
+            index 5794d472..772e2b99 100644
+            --- a/src/main/res/values-pt-rBR/strings.xml
+            +++ b/src/main/res/values-pt-rBR/strings.xml
+            @@ -1,3 +1,6 @@
+            <?xml version="1.0" encoding="UTF-8"?>
+            <resources xmlns:tools="http://schemas.android.com/tools">
+            +  <string name="video_quality">Video Quality</string>
+            +  <string name="popup_title">@string/app_name_title</string>
+            -  <string name="toast">@string/common_toast</string>
+            </resources>
+          STRINGS
+
+          ptbr_diff = GitDiffStruct.new('modified', ptbr_strings_xml, ptbr_xml_diff)
+
+          allow(@plugin.git).to receive(:diff_for_file).with(ptbr_strings_xml).and_return(ptbr_diff)
+
+          @plugin.check_strings_do_not_refer_resource
+
+          expected_warning = <<~WARNING
+            This PR adds a translatable entry which references another string resource; this usually causes issues with translations.
+            Please make sure to set the `translatable="false"` attribute:
+            File `#{main_strings_xml}`:
+            ```diff
+            +  <string name="screen_title">@string/app_name</string>
+            ```
+          WARNING
+
+          expected_warning2 = <<~WARNING
+            This PR adds a translatable entry which references another string resource; this usually causes issues with translations.
+            Please make sure to set the `translatable="false"` attribute:
+            File `#{main_strings_xml}`:
+            ```diff
+            +  <string name="screen_button">@string/button</string>
+            ```
+          WARNING
+
+          expected_warning3 = <<~WARNING
+            This PR adds a translatable entry which references another string resource; this usually causes issues with translations.
+            Please make sure to set the `translatable="false"` attribute:
+            File `#{ptbr_strings_xml}`:
+            ```diff
+            +  <string name="popup_title">@string/app_name_title</string>
+            ```
+          WARNING
+
+          expect(@dangerfile.status_report[:warnings]).to contain_exactly(expected_warning, expected_warning2, expected_warning3)
+        end
+
+        it 'does nothing when a PR adds a string resource reference inside a strings.xml file but with translatable=\"false\"' do
+          strings_xml_path = './src/main/res/values/strings.xml'
+          allow(@plugin.git).to receive(:modified_files).and_return([strings_xml_path])
+
+          strings_xml_diff = <<~STRINGS
+            diff --git a/src/main/res/values/strings.xml b/src/main/res/values/strings.xml
+            index 5794d472..772e2b99 100644
+            --- a/src/main/res/values/strings.xml
+            +++ b/src/main/res/values/strings.xml
+            @@ -1,3 +1,6 @@
+             <?xml version="1.0" encoding="UTF-8"?>
+             <resources xmlns:tools="http://schemas.android.com/tools">
+            +  <string name="select_categories">Select categories</string>
+            +  <string name="video_quality">Video Quality</string>
+            +  <string name="screen_title" translatable="false">@string/app_name</string>
+             </resources>
+          STRINGS
+
+          diff = GitDiffStruct.new('modified', strings_xml_path, strings_xml_diff)
+
+          allow(@plugin.git).to receive(:diff_for_file).with(strings_xml_path).and_return(diff)
+
+          @plugin.check_strings_do_not_refer_resource
+
+          expect(@dangerfile.status_report[:warnings]).to be_empty
+        end
+
+        it 'does nothing when a PR adds strings without resource references' do
+          strings_xml_path = './src/main/res/values/strings.xml'
+          allow(@plugin.git).to receive(:modified_files).and_return([strings_xml_path])
+
+          strings_xml_diff = <<~STRINGS
+            diff --git a/src/main/res/values/strings.xml b/src/main/res/values/strings.xml
+            index 5794d472..772e2b99 100644
+            --- a/src/main/res/values/strings.xml
+            +++ b/src/main/res/values/strings.xml
+            @@ -1,3 +1,6 @@
+             <?xml version="1.0" encoding="UTF-8"?>
+             <resources xmlns:tools="http://schemas.android.com/tools">
+            +  <string name="select_categories">Select categories</string>
+            +  <string name="video_quality">Video Quality</string>
+             </resources>
+          STRINGS
+
+          diff = GitDiffStruct.new('modified', strings_xml_path, strings_xml_diff)
+
+          allow(@plugin.git).to receive(:diff_for_file).with(strings_xml_path).and_return(diff)
+
+          @plugin.check_strings_do_not_refer_resource
+
+          expect(@dangerfile.status_report[:warnings]).to be_empty
+        end
+      end
+    end
+  end
+end
