87deeb74a0d3d94be60ffdfddd1db05eaf38daea
Ian Maia <ian.maia@automattic.com>
2023-07-27 18:26:07 +0200

    Fix faulty `danger_plugin.rb` merge

--- lib/danger_plugin.rb
+++ lib/danger_plugin.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-require 'dangermattic/plugins/android_unit_test_pr_checker'
+require 'dangermattic/plugins/android_unit_test_checker'
 require 'dangermattic/plugins/manifest_pr_checker'
 require 'dangermattic/plugins/milestone_checker'
 require 'dangermattic/plugins/pr_size_checker'
-require 'dangermattic/plugins/unit_test_pr_checker'
 require 'dangermattic/plugins/view_changes_need_screenshots'
