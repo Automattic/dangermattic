be4ac32f5f7785753aaa05ee0a106798f0818946
Ian Maia <ian.maia@automattic.com>
2023-07-31 18:22:20 +0200

    Add plugin for checking the diff and body text size of PRs

---
+++ lib/dangermattic/plugins/pr_size_checker.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+module Danger
+  # Plugin to check the size of a Pull Request content and text body.
+  class PRSizeChecker < Plugin
+    DEFAULT_MAX_DIFF_SIZE = 500
+    DEFAULT_DIFF_SIZE_MESSAGE = 'Please keep the Pull Request small, breaking it down into multiple ones if necessary'
+    DEFAULT_MIN_PR_BODY_MESSAGE = 'Please provide a summary of the changes in the Pull Request description'
+    DEFAULT_MIN_PR_BODY = 10
+
+    # Check the size of the PR diff against a specified maximum size.
+    #
+    # @param file_selector [Proc] Optional closure to filter the files in the diff to be used for size calculation.
+    # @param type [:insertions, :deletions, :all] The type of diff size to check. (default: :all)
+    # @param max_size [Integer] The maximum allowed size for the diff. (default: DEFAULT_MAX_DIFF_SIZE)
+    # @param message [String] The message to display if the diff size exceeds the maximum. (default: DEFAULT_DIFF_SIZE_MESSAGE)
+    # @param fail_on_error [Boolean] If true, fail the PR check when the diff size exceeds the maximum (default: false).
+    def check_diff_size(file_selector: nil, type: :all, max_size: DEFAULT_MAX_DIFF_SIZE, message: DEFAULT_DIFF_SIZE_MESSAGE, fail_on_error: false)
+      case type
+      when :insertions
+        if insertions_size(file_selector: file_selector) > max_size
+          fail_on_error ? failure(message) : warn(message)
+        end
+      when :deletions
+        if deletions_size(file_selector: file_selector) > max_size
+          fail_on_error ? failure(message) : warn(message)
+        end
+      when :all
+        if diff_size(file_selector: file_selector) > max_size
+          fail_on_error ? failure(message) : warn(message)
+        end
+      end
+    end
+
+    # Check the size of the Pull Request description (PR body) against a specified minimum size.
+    #
+    # @param min_length [Integer] The minimum allowed length for the PR body. (default: DEFAULT_MIN_PR_BODY)
+    # @param fail_on_error [Boolean] If true, fail the PR check when the PR body length is too small. (default: false)
+    def check_pr_body(min_length: DEFAULT_MIN_PR_BODY, message: DEFAULT_MIN_PR_BODY_MESSAGE, fail_on_error: false)
+      return if danger.github.pr_body.length > min_length
+
+      fail_on_error ? failure(message) : warn(message)
+    end
+
+    # Calculate the total size of insertions in modified files that match the file selector.
+    #
+    # @param file_selector [Proc] Select the files to be used for the insertions calculation.
+    # @return [Integer] The total size of insertions in the selected modified files.
+    def insertions_size(file_selector:)
+      return danger.git.insertions unless file_selector
+
+      filtered_files = all_modified_files.select(&file_selector)
+      filtered_files.sum { |file| danger.git.info_for_file(file)[:insertions].to_i }
+    end
+
+    # Calculate the total size of deletions in modified files that match the file selector.
+    #
+    # @param file_selector [Proc] Select the files to be used for the deletions calculation.
+    # @return [Integer] The total size of deletions in the selected modified files.
+    def deletions_size(file_selector:)
+      return danger.git.deletions unless file_selector
+
+      filtered_files = all_modified_files.select(&file_selector)
+      filtered_files.sum { |file| danger.git.info_for_file(file)[:deletions].to_i }
+    end
+
+    # Calculate the total size of changes (insertions and deletions) in modified files that match the file selector.
+    #
+    # @param file_selector [Proc] Select the files to be used for the total insertions and deletions calculation.
+    # @return [Integer] The total size of changes in the selected modified files.
+    def diff_size(file_selector:)
+      return danger.git.lines_of_code unless file_selector
+
+      filtered_files = all_modified_files.select(&file_selector)
+      filtered_files.sum { |file| danger.git.info_for_file(file)[:deletions].to_i + danger.git.info_for_file(file)[:insertions].to_i }
+    end
+
+    private
+
+    def all_modified_files
+      danger.git.added_files + danger.git.modified_files + danger.git.deleted_files
+    end
+  end
+end
---
+++ spec/pr_size_checker_spec.rb
@@ -0,0 +1,194 @@
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+
+module Danger
+  describe Danger::PRSizeChecker do
+    it 'is a plugin' do
+      expect(described_class.new(nil)).to be_a Danger::Plugin
+    end
+
+    describe 'with Dangerfile' do
+      before do
+        @dangerfile = testing_dangerfile
+        @plugin = @dangerfile.pr_size_checker
+      end
+
+      context 'when checking a PR diff size' do
+        before do
+          allow(@plugin.git).to receive(:added_files).and_return([])
+          allow(@plugin.git).to receive(:modified_files).and_return([])
+          allow(@plugin.git).to receive(:deleted_files).and_return([])
+        end
+
+        shared_examples 'using the default diff size counter without a file selector' do |type|
+          let(:diff_counter_for_type) do
+            type_hash = {
+              insertions: :insertions,
+              deletions: :deletions,
+              all: :lines_of_code
+            }
+
+            type_hash[type]
+          end
+
+          it 'returns a warning when using default parameters in a PR that has larger diff than the default maximum' do
+            allow(@plugin.git).to receive(diff_counter_for_type).and_return(501)
+
+            @plugin.check_diff_size(type: type)
+
+            expect(@dangerfile.status_report[:warnings]).to eq ['Please keep the Pull Request small, breaking it down into multiple ones if necessary']
+          end
+
+          it 'does nothing when using default parameters in a PR that has equal diff than the default maximum' do
+            allow(@plugin.git).to receive(diff_counter_for_type).and_return(500)
+
+            @plugin.check_diff_size(type: type)
+
+            expect(@dangerfile.status_report[:errors]).to be_empty
+            expect(@dangerfile.status_report[:warnings]).to be_empty
+          end
+
+          it 'does nothing when using default parameters in a PR that has smaller diff than the default maximum' do
+            allow(@plugin.git).to receive(diff_counter_for_type).and_return(499)
+
+            @plugin.check_diff_size(type: type)
+
+            expect(@dangerfile.status_report[:errors]).to be_empty
+            expect(@dangerfile.status_report[:warnings]).to be_empty
+          end
+
+          it 'returns an error using a custom message and a custom pr body size' do
+            allow(@plugin.git).to receive(diff_counter_for_type).and_return(600)
+
+            custom_message = 'diff size custom message'
+            @plugin.check_diff_size(type: type, max_size: 599, message: custom_message, fail_on_error: true)
+
+            expect(@dangerfile.status_report[:errors]).to eq [custom_message]
+          end
+        end
+
+        shared_examples 'using a file selector to filter and count the changes in a diff' do |type, max_sizes|
+          it 'returns a warning when using a files filter that will result in a diff that is too large' do
+            prepare_diff_with_test_files
+
+            @plugin.check_diff_size(
+              file_selector: ->(path) { File.dirname(path).start_with?('src/test/java') },
+              type: type,
+              max_size: max_sizes[0]
+            )
+
+            expect(@dangerfile.status_report[:warnings]).to eq ['Please keep the Pull Request small, breaking it down into multiple ones if necessary']
+          end
+
+          it 'returns a warning when using a files filter that will result in a diff that is too large, with a custom error' do
+            prepare_diff_with_test_files
+
+            custom_message = 'diff size too large custom file filter and error message'
+            @plugin.check_diff_size(
+              file_selector: ->(path) { File.extname(path) == '.java' },
+              type: type,
+              max_size: max_sizes[1],
+              message: custom_message,
+              fail_on_error: true
+            )
+
+            expect(@dangerfile.status_report[:warnings]).to be_empty
+            expect(@dangerfile.status_report[:errors]).to eq [custom_message]
+          end
+
+          it 'does nothing when a files filter is used but the max size is greater than or equal to the diff size' do
+            prepare_diff_with_test_files
+
+            @plugin.check_diff_size(
+              file_selector: ->(path) { File.extname(path).match(/^(.java|.kt)$/) },
+              type: type,
+              max_size: max_sizes[2]
+            )
+
+            expect(@dangerfile.status_report[:warnings]).to be_empty
+          end
+        end
+
+        context 'with the entire diff' do
+          include_examples 'using the default diff size counter without a file selector', :all
+          include_examples 'using a file selector to filter and count the changes in a diff', :all, [422, 520, 1541]
+        end
+
+        context 'with the insertions in the diff' do
+          include_examples 'using the default diff size counter without a file selector', :insertions
+          include_examples 'using a file selector to filter and count the changes in a diff', :insertions, [200, 139, 384]
+        end
+
+        context 'with the deletions in the diff' do
+          include_examples 'using the default diff size counter without a file selector', :deletions
+          include_examples 'using a file selector to filter and count the changes in a diff', :deletions, [221, 380, 1157]
+        end
+
+        def prepare_diff_with_test_files
+          added_test_file = 'src/test/java/org/magic/MagicTests.kt'
+          added_config = 'config.xml'
+          added_file = 'MyNewSorcery.java'
+          modified_file1 = 'src/java/PotionIngredients.java'
+          modified_file2 = 'src/java/Potion.kt'
+          modified_strings = 'src/main/res/values/strings.xml'
+          deleted_file1 = 'src/java/org/Fire.kt'
+          deleted_file2 = 'BlackCat.kt'
+          deleted_test_file = 'src/test/java/org/magic/Power.java'
+          deleted_strings = 'src/main/res/values-de/strings.xml'
+
+          allow(@plugin.git).to receive(:added_files).and_return([added_config, added_file])
+          allow(@plugin.git).to receive(:modified_files).and_return([modified_file1, modified_file2, added_test_file, modified_strings])
+          allow(@plugin.git).to receive(:deleted_files).and_return([deleted_file1, deleted_test_file, deleted_strings, deleted_file2])
+
+          allow(@plugin.git).to receive(:info_for_file).with(added_test_file).and_return({ insertions: 201 })
+          allow(@plugin.git).to receive(:info_for_file).with(added_config).and_return({ insertions: 311 })
+          allow(@plugin.git).to receive(:info_for_file).with(added_file).and_return({ insertions: 13 })
+          allow(@plugin.git).to receive(:info_for_file).with(modified_file1).and_return({ insertions: 127, deletions: 159 })
+          allow(@plugin.git).to receive(:info_for_file).with(modified_file2).and_return({ insertions: 43, deletions: 37 })
+          allow(@plugin.git).to receive(:info_for_file).with(modified_strings).and_return({ insertions: 432, deletions: 297 })
+          allow(@plugin.git).to receive(:info_for_file).with(deleted_file1).and_return({ deletions: 246 })
+          allow(@plugin.git).to receive(:info_for_file).with(deleted_file2).and_return({ deletions: 493 })
+          allow(@plugin.git).to receive(:info_for_file).with(deleted_test_file).and_return({ deletions: 222 })
+          allow(@plugin.git).to receive(:info_for_file).with(deleted_strings).and_return({ deletions: 593 })
+        end
+      end
+
+      context 'when checking a PR body size' do
+        it 'returns a warning when using default parameters in a PR that has a smaller body text length than the default minimum' do
+          allow(@plugin.github).to receive(:pr_body).and_return('pr body')
+
+          @plugin.check_pr_body
+
+          expect(@dangerfile.status_report[:warnings]).to eq ['Please provide a summary of the changes in the Pull Request description']
+        end
+
+        it 'does nothing when using default parameters in a PR that has a bigger pr body text length than the default minimum' do
+          allow(@plugin.github).to receive(:pr_body).and_return('some test pr body')
+
+          @plugin.check_pr_body
+
+          expect(@dangerfile.status_report[:warnings]).to be_empty
+        end
+
+        it 'returns an error when using default parameters in a PR that has an equal pr body text length than the default minimum' do
+          allow(@plugin.github).to receive(:pr_body).and_return('some test-')
+
+          @plugin.check_pr_body
+
+          expect(@dangerfile.status_report[:warnings]).to eq ['Please provide a summary of the changes in the Pull Request description']
+        end
+
+        it 'returns an error when using a custom message and a custom minimum pr body text length' do
+          allow(@plugin.github).to receive(:pr_body).and_return('still too short message')
+
+          custom_message = 'Custom error message'
+          @plugin.check_pr_body(min_length: 25, message: custom_message, fail_on_error: true)
+
+          expect(@dangerfile.status_report[:warnings]).to be_empty
+          expect(@dangerfile.status_report[:errors]).to eq [custom_message]
+        end
+      end
+    end
+  end
+end
