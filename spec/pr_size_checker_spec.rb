63e01fb98823beb32df276f96612eff92b1871f7
Ian Maia <ian.maia@automattic.com>
2023-08-01 14:45:27 +0200

    Add shared examples to cover more combinations when using custom warnings or errors

--- spec/pr_size_checker_spec.rb
+++ spec/pr_size_checker_spec.rb
@@ -21,7 +21,7 @@ module Danger
           allow(@plugin.git).to receive(:deleted_files).and_return([])
         end
 
-        shared_examples 'using the default diff size counter without a file selector' do |type|
+        shared_examples 'using the default diff size counter, without a file selector' do |type|
           let(:diff_counter_for_type) do
             type_hash = {
               insertions: :insertions,
@@ -38,7 +38,7 @@ module Danger
             @plugin.check_diff_size(type: type)
 
             expect(@dangerfile.status_report[:errors]).to be_empty
-            expect(@dangerfile.status_report[:warnings]).to eq ['This PR is larger than 500 lines of changes. Please consider splitting it into smaller PRs for easier and faster reviews.']
+            expect(@dangerfile.status_report[:warnings]).to eq [described_class::DEFAULT_DIFF_SIZE_MESSAGE]
           end
 
           it 'does nothing when using default parameters in a PR that has equal diff than the default maximum' do
@@ -59,14 +59,38 @@ module Danger
             expect(@dangerfile.status_report[:warnings]).to be_empty
           end
 
-          it 'reports an error using a custom message and a custom PR body size' do
-            allow(@plugin.git).to receive(diff_counter_for_type).and_return(600)
+          context 'when reporting a custom error or warning with a custom max_size' do
+            shared_examples 'reporting diff size custom warnings or errors' do |fail_on_error, message|
+              it 'reports an error using a custom message and a custom PR body size' do
+                allow(@plugin.git).to receive(diff_counter_for_type).and_return(600)
 
-            custom_message = 'diff size custom message'
-            @plugin.check_diff_size(type: type, max_size: 599, message: custom_message, fail_on_error: true)
+                if message
+                  @plugin.check_diff_size(type: type, max_size: 599, message: message, fail_on_error: fail_on_error)
+                else
+                  @plugin.check_diff_size(type: type, max_size: 599, fail_on_error: fail_on_error)
+                end
 
-            expect(@dangerfile.status_report[:warnings]).to be_empty
-            expect(@dangerfile.status_report[:errors]).to eq [custom_message]
+                message ||= described_class::DEFAULT_DIFF_SIZE_MESSAGE
+
+                expect_warning_or_error(fail_on_error: fail_on_error, message: message)
+              end
+            end
+
+            context 'when fail on error is false and a custom message is given' do
+              include_examples 'reporting diff size custom warnings or errors', false, 'this is my custom warning message'
+            end
+
+            context 'when fail on error is false' do
+              include_examples 'reporting diff size custom warnings or errors', false
+            end
+
+            context 'when fail on error is true' do
+              include_examples 'reporting diff size custom warnings or errors', true
+            end
+
+            context 'when a custom error message is given and fail on error is true' do
+              include_examples 'reporting diff size custom warnings or errors', true, 'this is my custom error message'
+            end
           end
         end
 
@@ -82,7 +106,7 @@ module Danger
               )
 
               expect(@dangerfile.status_report[:errors]).to be_empty
-              expect(@dangerfile.status_report[:warnings]).to eq ['This PR is larger than 500 lines of changes. Please consider splitting it into smaller PRs for easier and faster reviews.']
+              expect(@dangerfile.status_report[:warnings]).to eq [described_class::DEFAULT_DIFF_SIZE_MESSAGE]
             end
 
             it 'reports a custom error' do
@@ -114,49 +138,49 @@ module Danger
             expect(@dangerfile.status_report[:errors]).to be_empty
             expect(@dangerfile.status_report[:warnings]).to be_empty
           end
+
+          def prepare_diff_with_test_files
+            added_test_file = 'src/test/java/org/magic/MagicTests.kt'
+            added_config = 'config.xml'
+            added_file = 'MyNewSorcery.java'
+            modified_file1 = 'src/java/PotionIngredients.java'
+            modified_file2 = 'src/java/Potion.kt'
+            modified_strings = 'src/main/res/values/strings.xml'
+            deleted_file1 = 'src/java/org/Fire.kt'
+            deleted_file2 = 'BlackCat.kt'
+            deleted_test_file = 'src/test/java/org/magic/Power.java'
+            deleted_strings = 'src/main/res/values-de/strings.xml'
+
+            allow(@plugin.git).to receive(:added_files).and_return([added_config, added_file])
+            allow(@plugin.git).to receive(:modified_files).and_return([modified_file1, modified_file2, added_test_file, modified_strings])
+            allow(@plugin.git).to receive(:deleted_files).and_return([deleted_file1, deleted_test_file, deleted_strings, deleted_file2])
+
+            allow(@plugin.git).to receive(:info_for_file).with(added_test_file).and_return({ insertions: 201 })
+            allow(@plugin.git).to receive(:info_for_file).with(added_config).and_return({ insertions: 311 })
+            allow(@plugin.git).to receive(:info_for_file).with(added_file).and_return({ insertions: 13 })
+            allow(@plugin.git).to receive(:info_for_file).with(modified_file1).and_return({ insertions: 127, deletions: 159 })
+            allow(@plugin.git).to receive(:info_for_file).with(modified_file2).and_return({ insertions: 43, deletions: 37 })
+            allow(@plugin.git).to receive(:info_for_file).with(modified_strings).and_return({ insertions: 432, deletions: 297 })
+            allow(@plugin.git).to receive(:info_for_file).with(deleted_file1).and_return({ deletions: 246 })
+            allow(@plugin.git).to receive(:info_for_file).with(deleted_file2).and_return({ deletions: 493 })
+            allow(@plugin.git).to receive(:info_for_file).with(deleted_test_file).and_return({ deletions: 222 })
+            allow(@plugin.git).to receive(:info_for_file).with(deleted_strings).and_return({ deletions: 593 })
+          end
         end
 
         context 'with the entire diff' do
-          include_examples 'using the default diff size counter without a file selector', :all
+          include_examples 'using the default diff size counter, without a file selector', :all
           include_examples 'using a file selector to filter and count the changes in a diff', :all, [422, 520, 1541]
         end
 
         context 'with the insertions in the diff' do
-          include_examples 'using the default diff size counter without a file selector', :insertions
+          include_examples 'using the default diff size counter, without a file selector', :insertions
           include_examples 'using a file selector to filter and count the changes in a diff', :insertions, [200, 139, 384]
         end
 
         context 'with the deletions in the diff' do
-          include_examples 'using the default diff size counter without a file selector', :deletions
+          include_examples 'using the default diff size counter, without a file selector', :deletions
           include_examples 'using a file selector to filter and count the changes in a diff', :deletions, [221, 380, 1157]
-        end
-
-        def prepare_diff_with_test_files
-          added_test_file = 'src/test/java/org/magic/MagicTests.kt'
-          added_config = 'config.xml'
-          added_file = 'MyNewSorcery.java'
-          modified_file1 = 'src/java/PotionIngredients.java'
-          modified_file2 = 'src/java/Potion.kt'
-          modified_strings = 'src/main/res/values/strings.xml'
-          deleted_file1 = 'src/java/org/Fire.kt'
-          deleted_file2 = 'BlackCat.kt'
-          deleted_test_file = 'src/test/java/org/magic/Power.java'
-          deleted_strings = 'src/main/res/values-de/strings.xml'
-
-          allow(@plugin.git).to receive(:added_files).and_return([added_config, added_file])
-          allow(@plugin.git).to receive(:modified_files).and_return([modified_file1, modified_file2, added_test_file, modified_strings])
-          allow(@plugin.git).to receive(:deleted_files).and_return([deleted_file1, deleted_test_file, deleted_strings, deleted_file2])
-
-          allow(@plugin.git).to receive(:info_for_file).with(added_test_file).and_return({ insertions: 201 })
-          allow(@plugin.git).to receive(:info_for_file).with(added_config).and_return({ insertions: 311 })
-          allow(@plugin.git).to receive(:info_for_file).with(added_file).and_return({ insertions: 13 })
-          allow(@plugin.git).to receive(:info_for_file).with(modified_file1).and_return({ insertions: 127, deletions: 159 })
-          allow(@plugin.git).to receive(:info_for_file).with(modified_file2).and_return({ insertions: 43, deletions: 37 })
-          allow(@plugin.git).to receive(:info_for_file).with(modified_strings).and_return({ insertions: 432, deletions: 297 })
-          allow(@plugin.git).to receive(:info_for_file).with(deleted_file1).and_return({ deletions: 246 })
-          allow(@plugin.git).to receive(:info_for_file).with(deleted_file2).and_return({ deletions: 493 })
-          allow(@plugin.git).to receive(:info_for_file).with(deleted_test_file).and_return({ deletions: 222 })
-          allow(@plugin.git).to receive(:info_for_file).with(deleted_strings).and_return({ deletions: 593 })
         end
       end
 
@@ -167,7 +191,7 @@ module Danger
           @plugin.check_pr_body
 
           expect(@dangerfile.status_report[:errors]).to be_empty
-          expect(@dangerfile.status_report[:warnings]).to eq ['The PR description appears very short, less than 10 characters long. Please provide a summary of your changes in the PR description.']
+          expect(@dangerfile.status_report[:warnings]).to eq [described_class::DEFAULT_MIN_PR_BODY_MESSAGE]
         end
 
         it 'does nothing when using default parameters in a PR that has a bigger PR body text length than the default minimum' do
@@ -185,17 +209,51 @@ module Danger
           @plugin.check_pr_body
 
           expect(@dangerfile.status_report[:errors]).to be_empty
-          expect(@dangerfile.status_report[:warnings]).to eq ['The PR description appears very short, less than 10 characters long. Please provide a summary of your changes in the PR description.']
+          expect(@dangerfile.status_report[:warnings]).to eq [described_class::DEFAULT_MIN_PR_BODY_MESSAGE]
         end
 
-        it 'reports an error when using a custom message and a custom minimum PR body text length' do
-          allow(@plugin.github).to receive(:pr_body).and_return('still too short message')
+        context 'when reporting a custom error or warning with a custom min_length' do
+          shared_examples 'reporting PR length check custom warnings or errors' do |fail_on_error, message|
+            it 'reports an error when using a custom message and a custom minimum PR body text length' do
+              allow(@plugin.github).to receive(:pr_body).and_return('still too short message')
 
-          custom_message = 'Custom error message'
-          @plugin.check_pr_body(min_length: 25, message: custom_message, fail_on_error: true)
+              if message
+                @plugin.check_pr_body(min_length: 25, message: message, fail_on_error: fail_on_error)
+              else
+                @plugin.check_pr_body(min_length: 25, fail_on_error: fail_on_error)
+              end
 
+              message ||= described_class::DEFAULT_MIN_PR_BODY_MESSAGE
+
+              expect_warning_or_error(fail_on_error: fail_on_error, message: message)
+            end
+          end
+
+          context 'when fail on error is false and a custom message is given' do
+            include_examples 'reporting PR length check custom warnings or errors', false, 'this is my custom warning message'
+          end
+
+          context 'when fail on error is false' do
+            include_examples 'reporting PR length check custom warnings or errors', false
+          end
+
+          context 'when fail on error is true' do
+            include_examples 'reporting PR length check custom warnings or errors', true
+          end
+
+          context 'when a custom error message is given and fail on error is true' do
+            include_examples 'reporting PR length check custom warnings or errors', true, 'this is my custom error message'
+          end
+        end
+      end
+
+      def expect_warning_or_error(fail_on_error:, message:)
+        if fail_on_error
           expect(@dangerfile.status_report[:warnings]).to be_empty
-          expect(@dangerfile.status_report[:errors]).to eq [custom_message]
+          expect(@dangerfile.status_report[:errors]).to eq [message]
+        else
+          expect(@dangerfile.status_report[:warnings]).to eq [message]
+          expect(@dangerfile.status_report[:errors]).to be_empty
         end
       end
     end
