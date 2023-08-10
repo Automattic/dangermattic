8b633e5047ccb584d26d632fd7adb92db00a7890
Ian Maia <ian.maia@automattic.com>
2023-08-10 20:12:50 +0200

    Add plugin for checking the tracks usage

---
+++ lib/dangermattic/plugins/check_tracks.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+module Danger
+  # Plugin for checking tracks-related changes and providing instructions in the PR if that's the case.
+  class CheckTracks < Plugin
+    DEFAULT_TRACKS_FILES = [
+      'AnalyticsTracker.kt',
+      'AnalyticsEvent.kt',
+      'LoginAnalyticsTracker.kt',
+      'WooAnalyticsStat.swift'
+    ].freeze
+
+    DEFAULT_TRACKS_USE_MATCHERS = [
+      /AnalyticsTracker\.track/
+    ].freeze
+
+    TRACKS_PR_INSTRUCTIONS = <<~MESSAGE
+      This PR contains changes to Tracks-related logic. Please ensure the following are completed:
+      **PR Author**
+      - The PR must be assigned the **Tracks** label
+      **PR Reviewer**
+      - The tracks events must be validated in the Tracks system.
+      - Verify the internal tracks spreadsheet has also been updated.
+    MESSAGE
+
+    # Checks the PR diff for changes in tracks-related files and provides instructions if changes are detected
+    #
+    # @param tracks_files [Array<String>] List of tracks-related file names to check (default: DEFAULT_TRACKS_FILES)
+    # @param tracks_usage_match [Array<Regexp>] List of regular expressions representing tracks usages to match the diff lines (default: DEFAULT_TRACKS_FILES)
+    # @return [void]
+    def check_tracks_changes(tracks_files: DEFAULT_TRACKS_FILES, tracks_usage_matchers: DEFAULT_TRACKS_USE_MATCHERS)
+      return unless changes_tracks_files?(tracks_files: tracks_files) || diff_has_tracks_changes?(tracks_usage_matchers: tracks_usage_matchers)
+
+      # tracks related changes detected: publishing instructions
+      message(TRACKS_PR_INSTRUCTIONS)
+    end
+
+    private
+
+    def changes_tracks_files?(tracks_files:)
+      git_utils.all_changed_files.any? do |file|
+        tracks_files.any? { |tracks_file| File.basename(file) == File.basename(tracks_file) }
+      end
+    end
+
+    def diff_has_tracks_changes?(tracks_usage_matchers:)
+      matched_lines = git_utils.match_diff_lines_in_files(
+        files: git_utils.all_changed_files,
+        line_matcher: ->(line) { tracks_usage_matchers.any? { |tracks_usage_match| line.match(tracks_usage_match) } },
+        change_type: nil
+      )
+
+      !matched_lines.empty?
+    end
+  end
+end
---
+++ spec/check_tracks_spec.rb
@@ -0,0 +1,132 @@
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+
+module Danger
+  describe Danger::CheckTracks do
+    it 'is a plugin' do
+      expect(described_class.new(nil)).to be_a Danger::Plugin
+    end
+
+    describe 'with Dangerfile' do
+      before do
+        @dangerfile = testing_dangerfile
+        @plugin = @dangerfile.check_tracks
+      end
+
+      describe '#check_tracks_changes' do
+        before do
+          allow(@plugin.git).to receive(:modified_files).and_return([])
+          allow(@plugin.git).to receive(:added_files).and_return([])
+          allow(@plugin.git).to receive(:deleted_files).and_return([])
+
+          stub_const('GitDiffStruct', Struct.new(:type, :path, :patch))
+        end
+
+        context 'when checking changes in track related files' do
+          it 'reports a message with instructions for review when there are changes in tracks files' do
+            allow(@plugin.git_utils).to receive(:all_changed_files).and_return(['Test.kt', 'LoginAnalyticsTracker.kt', 'Test.java'])
+
+            @plugin.check_tracks_changes
+
+            expect(@dangerfile.status_report[:messages]).to eq [CheckTracks::TRACKS_PR_INSTRUCTIONS]
+          end
+
+          it 'reports a message with instructions for review when there are changes in tracks files using a custom file list' do
+            allow(@plugin.git_utils).to receive(:all_changed_files).and_return(['MyClass.swift', 'MyClass1.swift', 'MyClass2.swift'])
+
+            @plugin.check_tracks_changes(tracks_files: ['MyClass1.swift'])
+
+            expect(@dangerfile.status_report[:messages]).to eq [CheckTracks::TRACKS_PR_INSTRUCTIONS]
+          end
+
+          it 'does nothing when there are no changes in tracks files' do
+            modified_files = ['MyClass.swift']
+            allow(@plugin.git_utils).to receive(:all_changed_files).and_return(modified_files)
+            allow(@plugin.git_utils).to receive(:match_diff_lines_in_files).with(files: modified_files, line_matcher: kind_of(Proc), change_type: nil).and_return([])
+
+            @plugin.check_tracks_changes
+
+            expect(@dangerfile.status_report[:messages]).to be_empty
+          end
+
+          it 'does nothing when there are no changes in tracks files using a custom file list' do
+            modified_files = ['MyClass.swift']
+            allow(@plugin.git_utils).to receive(:all_changed_files).and_return(modified_files)
+            allow(@plugin.git_utils).to receive(:match_diff_lines_in_files).with(files: modified_files, line_matcher: kind_of(Proc), change_type: nil).and_return([])
+
+            @plugin.check_tracks_changes(tracks_files: ['MyClass1.swift'])
+
+            expect(@dangerfile.status_report[:messages]).to be_empty
+          end
+        end
+
+        context 'when checking track related changes within a diff' do
+          it 'reports a message with instructions for review when there are matching changes' do
+            modified_files = ['MyClass.kt']
+            allow(@plugin.git_utils).to receive(:all_changed_files).and_return(modified_files)
+
+            allow(@plugin.git_utils).to receive(:match_diff_lines_in_files).with(files: modified_files, line_matcher: kind_of(Proc), change_type: nil) do |args|
+              analytics_call_in_diff = '-                AnalyticsTracker.track("myEvent1")'
+              expect(args[:line_matcher].call(analytics_call_in_diff)).to be true
+
+              [analytics_call_in_diff]
+            end
+
+            @plugin.check_tracks_changes
+
+            expect(@dangerfile.status_report[:messages]).to eq [CheckTracks::TRACKS_PR_INSTRUCTIONS]
+          end
+
+          it 'reports a message with instructions for review when there are changes using a custom line matcher expression' do
+            modified_files = ['MyClass.kt']
+            allow(@plugin.git_utils).to receive(:all_changed_files).and_return(modified_files)
+
+            allow(@plugin.git_utils).to receive(:match_diff_lines_in_files).with(files: modified_files, line_matcher: kind_of(Proc), change_type: nil) do |args|
+              analytics_call_in_diff = '+                AnalyticsHelper.log("event_1")'
+              expect(args[:line_matcher].call(analytics_call_in_diff)).to be true
+
+              [analytics_call_in_diff]
+            end
+
+            @plugin.check_tracks_changes(tracks_usage_matchers: [/AnalyticsHelper\.log/])
+
+            expect(@dangerfile.status_report[:messages]).to eq [CheckTracks::TRACKS_PR_INSTRUCTIONS]
+          end
+
+          it 'does nothing when there are no matching changes' do
+            modified_files = ['MyClass.kt']
+            allow(@plugin.git_utils).to receive(:all_changed_files).and_return(modified_files)
+
+            allow(@plugin.git_utils).to receive(:match_diff_lines_in_files).with(files: modified_files, line_matcher: kind_of(Proc), change_type: nil) do |args|
+              analytics_call_in_diff = '+                AnalyticsHelper.log("event_1")'
+              expect(args[:line_matcher].call(analytics_call_in_diff)).to be false
+
+              []
+            end
+
+            @plugin.check_tracks_changes
+
+            expect(@dangerfile.status_report[:messages]).to be_empty
+          end
+
+          it 'does nothing when there are no matching changes using a custom matcher' do
+            modified_files = ['MyClass.kt']
+            allow(@plugin.git_utils).to receive(:all_changed_files).and_return(modified_files)
+
+            allow(@plugin.git_utils).to receive(:match_diff_lines_in_files).with(files: modified_files, line_matcher: kind_of(Proc), change_type: nil) do |args|
+              analytics_call_in_diff = '+                AnalyticsTracker.track("myEvent1")'
+              expect(args[:line_matcher].call(analytics_call_in_diff)).to be false
+
+              []
+            end
+
+            @plugin.check_tracks_changes(tracks_usage_matchers: [/AnalyticsHelper\.log$/])
+
+            expect(@dangerfile.status_report[:messages]).to be_empty
+          end
+        end
+      end
+    end
+  end
+end
