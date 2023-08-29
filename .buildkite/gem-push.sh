070f346e22d1b5e03f978e71e8106b7ad1cc57d7
Ian Maia <ian.maia@automattic.com>
2023-08-29 17:25:24 +0200

    Add script to publish Gem to RubyGems

---
+++ .buildkite/gem-push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+GEM_NAME="danger-dangermattic"
+
+echo "--- :hammer: Build Gemspec"
+gem build "$GEM_NAME.gemspec" -o "$GEM_NAME.gem"
+
+echo "--- :sleuth_or_spy: Validate Gem Install"
+gem install --user-install "$GEM_NAME.gem"
+
+echo "--- :rubygems: Gem Push"
+echo ":rubygems_api_key: ${RUBYGEMS_API_KEY}" >>".gem-credentials"
+chmod 600 ".gem-credentials"
+gem push --config-file ".gem-credentials" "$GEM_NAME.gem"
+
