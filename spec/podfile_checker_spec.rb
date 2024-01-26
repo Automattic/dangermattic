d92e8a267a84bbb5c89d034b88c2974ee4a797f7
Ian Maia <ian.maia@automattic.com>
2024-01-26 15:57:37 +0100

    Rename podfile checker

--- spec/podfile_checker_spec.rb
+++ spec/podfile_checker_spec.rb
@@ -3,7 +3,7 @@
 require_relative 'spec_helper'
 
 module Danger
-  describe Danger::PodfileChecks do
+  describe Danger::PodfileChecker do
     it 'is a plugin' do
       expect(described_class.new(nil)).to be_a Danger::Plugin
     end
@@ -11,7 +11,7 @@ module Danger
     describe 'with Dangerfile' do
       before do
         @dangerfile = testing_dangerfile
-        @plugin = @dangerfile.podfile_checks
+        @plugin = @dangerfile.podfile_checker
 
         allow(@plugin.git).to receive_messages(added_files: [], modified_files: [], deleted_files: [])
 
