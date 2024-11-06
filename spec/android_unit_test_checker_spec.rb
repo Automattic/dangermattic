50ba674e7d09e771de12ff5eb0467247ffbbb14c
Michał Sikora <me@mehow.io>
2024-11-06 13:02:11 +0100

    Revert commenting out length assertion

--- spec/android_unit_test_checker_spec.rb
+++ spec/android_unit_test_checker_spec.rb
@@ -348,7 +348,7 @@ module Danger
     end
 
     def expect_class_names_match_report(class_names:, error_report:)
-      # expect(class_names.length).to eq(error_report.length)
+      expect(class_names.length).to eq(error_report.length)
       class_names.zip(error_report).each do |cls, error|
         expect(error).to include "Please add tests for class `#{cls}`"
       end
