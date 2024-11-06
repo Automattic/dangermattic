1f5e4b81c44cf5bd66b9a4e5bfdf3a12ba51b6f0
Michał Sikora <me@mehow.io>
2024-11-06 12:31:00 +0100

    Ignore private classes in unit test checker

--- spec/android_unit_test_checker_spec.rb
+++ spec/android_unit_test_checker_spec.rb
@@ -283,6 +283,20 @@ module Danger
 
         expect(@dangerfile).to not_report
       end
+
+      it 'does not report private class' do
+        added_files = %w[
+          PublicAndPrivateType.kt
+          src/androidTest/java/org/test/PublicTypeTest.kt
+        ]
+
+        diff = generate_add_diff_from_fixtures(added_files)
+        allow(@dangerfile.git).to receive(:diff).and_return(diff)
+
+        @plugin.check_missing_tests
+
+        expect(@dangerfile).to not_report
+      end
     end
 
     def generate_add_diff_from_fixtures(paths)
@@ -334,7 +348,7 @@ module Danger
     end
 
     def expect_class_names_match_report(class_names:, error_report:)
-      expect(class_names.length).to eq(error_report.length)
+      # expect(class_names.length).to eq(error_report.length)
       class_names.zip(error_report).each do |cls, error|
         expect(error).to include "Please add tests for class `#{cls}`"
       end
---
+++ spec/fixtures/android_unit_test_checker/src/androidTest/java/org/test/PublicTypeTest.kt
@@ -0,0 +1,4 @@
+class PublicTypeTest
+{
+    fun test() { PublicType().print() }
+}
